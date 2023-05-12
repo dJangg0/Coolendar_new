a349387eba36de85ed38847ac1d89dafde615809
dJangg0 <kalamayblue@gmail.com>
2023-05-13 00:24:01 +0800

    No alarm management and notification

--- app/src/main/java/com/example/coolendar/MainActivity.java
+++ app/src/main/java/com/example/coolendar/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.coolendar;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        ImageButton btnAdd = findViewById(R.id.btnAdd);
+        ImageButton back = findViewById(R.id.back);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alarmSet();
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStartingActivity();
+            }
+        });
+
+    } //closing oncreate brackets
+
+    private void openStartingActivity() {
+        Intent intent = new Intent(this,StartingActivity.class);
+        startActivity(intent);
     }
-}
+
+    public void alarmSet(){
+      Intent intent = new Intent(this,AlarmSet.class);
+      startActivity(intent);
+
+    }
+
+
+
+} //closing brackets
--- app/src/main/java/com/example/coolendar/StartingActivity.java
+++ app/src/main/java/com/example/coolendar/StartingActivity.java
@@ -1,21 +1,21 @@
 package com.example.coolendar;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-
-import android.os.Bundle;
-
 public class StartingActivity extends AppCompatActivity {
-    Button button;
+    ImageButton button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_starting);
-        button = (Button) findViewById(R.id.button);
+        setContentView(R.layout.activity_introduction);
+        button = (ImageButton) findViewById(R.id.imgBtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
